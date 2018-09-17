be804ca0140bbb816955d071e8ca5857afdbe966
Grant <g.free697@gmail.com>
2018-09-16 20:14:02 -0500

    added city lookup

--- weather.py
+++ weather.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3.6
 import requests
-from weatherutil import *
 from pprint import pprint
-# variables
+import json
+import re
+
+city = input("Where are you located? (Ex. Los Angeles, London, Paris)\n")
+
+matches = []
+with open('city.list.json', 'r') as city_json:
+    json_list = json.load(city_json)
+    for i in json_list:
+        if re.fullmatch(city.lower(), i['name'].lower()):
+            matches.append(i)
+
 r = requests.get(
-    'http://api.openweathermap.org/data/2.5/weather?id=4339348&appid=beb929e6e8c5c36b61aa990cfbd90558')
+    'http://api.openweathermap.org/data/2.5/weather?id={}&appid=beb929e6e8c5c36b61aa990cfbd90558'.format(matches[0]['id']))
 r_data = r.json()
 temp_f = (r_data['main']['temp'] - 273.15) * (9 / 5) + 32
 wind_speed = (r_data['wind']['speed']) * (25 / 11)
 
-def printweather():
-    print("Current temperature in %s is %s degrees." %
-          (r_data['name'], round(temp_f, 1)))
-    print("Humidity is %s%%." % (r_data['main']['humidity']))
-    print("Wind is currently blowing %s at %s mph" %
-          (deg_to_dir(r_data['wind']['deg']), round(wind_speed, 1)))
-
-printweather()
+print(
+    f"Current temperature in {r_data['name']} is {round(temp_f, 1)} degrees.")
+print(f"Humidity is {r_data['main']['humidity']}%.")
