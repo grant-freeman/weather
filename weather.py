55b4363ba9eac7466f91041345feb11fe26db49e
Grant <g.free697@gmail.com>
2018-10-30 11:03:28 -0500

    added multiple city handling. command line usage. usage statement.

--- weather.py
+++ weather.py
@@ -3,22 +3,34 @@ import requests
 from pprint import pprint
 import json
 import re
+import sys
 
-city = input("Where are you located? (Ex. Los Angeles, London, Paris)\n")
+usage = "usage: ./weather.py city1 [city2 ...]\n    city1, city2, etc: cities to look up"
 
-matches = []
-with open('city.list.json', 'r') as city_json:
-    json_list = json.load(city_json)
-    for i in json_list:
-        if re.fullmatch(city.lower(), i['name'].lower()):
-            matches.append(i)
+if len(sys.argv) < 2:
+    print(usage)
+    exit()
+else:
+    city_list = sys.argv[1:]
 
-r = requests.get(
-    'http://api.openweathermap.org/data/2.5/weather?id={}&appid=beb929e6e8c5c36b61aa990cfbd90558'.format(matches[0]['id']))
-r_data = r.json()
-temp_f = (r_data['main']['temp'] - 273.15) * (9 / 5) + 32
-wind_speed = (r_data['wind']['speed']) * (25 / 11)
 
-print(
-    f"Current temperature in {r_data['name']} is {round(temp_f, 1)} degrees.")
-print(f"Humidity is {r_data['main']['humidity']}%.")
+def weather(city):
+    matches = []
+    with open('city.list.json', 'r') as city_json:
+        json_list = json.load(city_json)
+        for i in json_list:
+            if re.fullmatch(city.lower(), i['name'].lower()):
+                matches.append(i)
+
+    r = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?id={}&appid=beb929e6e8c5c36b61aa990cfbd90558'.format(matches[0]['id']))
+    r_data = r.json()
+    temp_f = (r_data['main']['temp'] - 273.15) * (9 / 5) + 32
+
+    print(
+        f"{r_data['name']}:\n  -temperature: {round(temp_f, 1)}°")
+    print(f"  -humidity: {r_data['main']['humidity']}%")
+
+
+for i in city_list:
+    weather(i)
